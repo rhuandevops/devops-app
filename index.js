3c6a33604960fa4d01372c7cd91faf47fe072e58
rhuandevops <rhuanbr_@hotmail.com>
2025-05-03 17:44:25 -0300

    Remove build Docker para compatibilidade com Railway

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.get("/", (req, res) => res.send("Hello, DevOps 2.0!"));
 
-// Só dá listen se for execução direta (ok para Railway E para testes)
+// ✅ Railway e local funcionam, Jest não dá conflito
 if (require.main === module) {
   const port = process.env.PORT || 3000;
   app.listen(port, () => console.log(`App rodando na porta ${port}`));
