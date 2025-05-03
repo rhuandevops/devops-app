d9914c2de20a59d8a793126778a194357c27a5d2
rhuandevops <rhuanbr_@hotmail.com>
2025-05-03 17:02:13 -0300

    Atualiza teste para nova resposta da API

--- index.test.js
+++ index.test.js
@@ -5,6 +5,6 @@ describe("GET /", () => {
   it("deve responder com 'Hello, DevOps!'", async () => {
     const response = await request(app).get("/");
     expect(response.statusCode).toBe(200);
-    expect(response.text).toBe("Hello, DevOps!");
+    expect(response.text).toBe("Hello, DevOps 2.0!");
   });
 });
