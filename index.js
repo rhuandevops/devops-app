d9850c3118623a70e7bb57129fa84d92e6070c99
rhuandevops <rhuanbr_@hotmail.com>
2025-05-03 17:09:19 -0300

    Corrige execução do servidor para Railway

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ const app = express();
 
 app.get("/", (req, res) => res.send("Hello, DevOps 2.0!"));
 
-if (require.main === module) {
-  const port = process.env.PORT || 3000;
-  app.listen(port, () => console.log(`App rodando na porta ${port}`));
-}
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`App rodando na porta ${port}`));
 
 module.exports = app;
