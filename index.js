21d2d4079613e91235bdd95adc3fd1e3e2ff643d
rhuandevops <rhuanbr_@hotmail.com>
2025-05-03 16:36:19 -0300

    Adiciona etapa de testes com Jest no pipeline

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-// index.js
 const express = require("express");
 const app = express();
+
 app.get("/", (req, res) => res.send("Hello, DevOps!"));
-app.listen(3000, () => console.log("App rodando na porta 3000"));
+
+if (require.main === module) {
+  app.listen(3000, () => console.log("App rodando na porta 3000"));
+}
+
+module.exports = app;
