b0ce293fe3b2cce34bec69b129cd1439ba28a8d5
rhuandevops <rhuanbr_@hotmail.com>
2025-05-03 17:15:04 -0300

    Remove verificação de execução direta para Railway

--- index.test.js
+++ index.test.js
@@ -2,7 +2,7 @@ const request = require("supertest");
 const app = require("./index");
 
 describe("GET /", () => {
-  it("deve responder com 'Hello, DevOps!'", async () => {
+  it("deve responder com 'Hello, DevOps 2.0!'", async () => {
     const response = await request(app).get("/");
     expect(response.statusCode).toBe(200);
     expect(response.text).toBe("Hello, DevOps 2.0!");
