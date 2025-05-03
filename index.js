1211fa7f5e7243e62c97ec6b4bc2daac4b65f439
rhuandevops <rhuanbr_@hotmail.com>
2025-05-03 16:59:38 -0300

    Atualiza resposta da API

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello, DevOps!"));
+app.get("/", (req, res) => res.send("Hello, DevOps 2.0!"));
 
 if (require.main === module) {
   const port = process.env.PORT || 3000;
